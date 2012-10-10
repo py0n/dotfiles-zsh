cd2093c59fb722596321a869b0e6ca8060a38cb7
ISHIKAWA YUICHIRO <p.y.o.n.1.3+git@gmail.com>
2012-10-10 10:12:47 +0900

    Agent forwardの時にも対応。

--- .zsh.d/sshagent.zsh
+++ .zsh.d/sshagent.zsh
@@ -1,14 +1,13 @@
 # ssh-agent
 # http://blog.gcd.org/archives/50713097.html
-if [ ! -z ${SSH_AGENT_PID} ]; then
-	AGENT=${HOME}/.ssh-agent-${USER}
-	if [ -S "${SSH_AUTH_SOCK}" -a ! -L "${SSH_AUTH_SOCK}" ]; then
-		ln -sfn ${SSH_AUTH_SOCK} ${AGENT} && export SSH_AUTH_SOCK=${AGENT}
-	elif [ -S "${AGENT}" ]; then
-		export SSH_AUTH_SOCK=${AGENT}
-	else
-		echo "no ssh-agent"
-	fi
+
+AGENT=${HOME}/.ssh-agent-${USER}
+if [ -S "${SSH_AUTH_SOCK}" -a ! -L "${SSH_AUTH_SOCK}" ]; then
+    ln -sfn ${SSH_AUTH_SOCK} ${AGENT} && export SSH_AUTH_SOCK=${AGENT}
+elif [ -S "${AGENT}" ]; then
+    export SSH_AUTH_SOCK=${AGENT}
+elif [ ! -z ${SSH_AGENT_PID} ]; then
+    echo "no ssh-agent"
 fi
 
 # vim: expandtab filetype=zsh shiftwidth=4 softtabstop=4 ts=4
