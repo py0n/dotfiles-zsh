c7da8c7cde4ead4130dc5199279cd4658ee2ea4b
py0n <p.y.o.n.1.3+git@gmail.com>
2013-09-12 17:02:56 +0900

    Cygwinのssh-pageantに対応。
    http://cuviper.github.io/ssh-pageant/

--- sshagent.zsh
+++ sshagent.zsh
@@ -2,11 +2,29 @@
 # http://blog.gcd.org/archives/50713097.html
 
 AGENT=${HOME}/.ssh-agent-${USER}
-if [ -S "${SSH_AUTH_SOCK}" -a ! -L "${SSH_AUTH_SOCK}" ]; then
-    ln -sfn ${SSH_AUTH_SOCK} ${AGENT} && export SSH_AUTH_SOCK=${AGENT}
-elif [ -S "${AGENT}" ]; then
-    export SSH_AUTH_SOCK=${AGENT}
-elif [ ! -z ${SSH_AGENT_PID} ]; then
+
+UNAME=`uname`
+case "$UNAME" in
+    CYGWIN*)
+        SSH_PAGEANT=`which ssh-pageant`
+
+        killall -0 $SSH_PAGEANT 2>/dev/null && killall $SSH_PAGEANT
+
+        eval $($SSH_PAGEANT -q)
+        ln -sfn $SSH_AUTH_SOCK $AGENT && export SSH_AUTH_SOCK=$AGENT
+
+        #trap 'eval $($SSH_PAGEANT -qk 2>/dev/null)' EXIT
+        trap 'logout' HUP
+        ;;
+    *)
+        ;;
+esac
+
+if [ -S "$SSH_AUTH_SOCK" -a ! -L "$SSH_AUTH_SOCK" ]; then
+    ln -sfn $SSH_AUTH_SOCK $AGENT && export SSH_AUTH_SOCK=$AGENT
+elif [ -S "$AGENT" ]; then
+    export SSH_AUTH_SOCK=$AGENT
+else
     echo "no ssh-agent"
 fi
 
