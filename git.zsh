4824ef13e5ba453643076c8cff55d3cd84e9902e
ISHIKAWA YUICHIRO <p.y.o.n.1.3+git@gmail.com>
2014-01-31 16:13:54 +0900

    記述をコンパクトに。

--- git.zsh
+++ git.zsh
@@ -6,23 +6,21 @@ git_config() {
     local LV=`which lv`
     local NKF=`which nkf`
 
-    if [[ -n "$GIT" && -x "$GIT" && -n "$DIFF" && -x "$DIFF" ]]; then
-        if [[ -n "$LESS" && -x "$LESS" ]]; then
-            git config --global --replace-all color.ui true
-            if [[ -n "$NKF" && -x "$NKF" ]]; then
-                git config --global --replace-all pager.diff "diff-highlight | nkf -w | LESS=-r less"
-                git config --global --replace-all pager.log "diff-highlight | nkf -w | LESS=-r less"
-                git config --global --replace-all pager.show "diff-highlight | nkf -w | LESS=-r less"
-            elif [[ -n "$LV" && -x "$LV" ]]; then
-                git config --global --replace-all pager.diff "diff-highlight | lv -Ou | LESS=-r less"
-                git config --global --replace-all pager.log "diff-highlight | lv -Ou | LESS=-r less"
-                git config --global --replace-all pager.show "diff-highlight | lv -Ou | LESS=-r less"
-            else
-                git config --global --replace-all pager.diff "diff-highlight | LESS=-r less"
-                git config --global --replace-all pager.log "diff-highlight | LESS=-r less"
-                git config --global --replace-all pager.show "diff-highlight | LESS=-r less"
-            fi
+    if [[ -x "$GIT" && -x "$DIFF" && -x "$LESS" ]]; then
+        # 色設定。
+        git config --global --replace-all color.ui true
+
+        # viewer設定。
+        if [[ -x "$NKF" ]]; then
+            local PAGER="diff-highlight | nkf -w | LESS=-R less"
+        elif [[ -x "$LV" ]]; then
+            local PAGER="diff-highlight | lv -Ou | LESS=-R less"
+        else
+            local PAGER="diff-highlight | LESS=-R less"
         fi
+        git config --global --replace-all pager.diff "$PAGER"
+        git config --global --replace-all pager.log "$PAGER"
+        git config --global --replace-all pager.show "$PAGER"
     fi
 }
 
